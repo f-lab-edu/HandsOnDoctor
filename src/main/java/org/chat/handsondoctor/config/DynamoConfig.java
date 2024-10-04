a635e9ec72fbe091f10407034c51414f9ca979ad
BangOol <yjg02211@naver.com>
2024-10-04 10:57:02 +0900

    [#6] DynamoDB config 수정
    ## 수정 사항
    - 내부에서 IAM을 통해 DynamoDB 권한 확인하도록 수정.

--- src/main/java/org/chat/handsondoctor/config/DynamoConfig.java
+++ src/main/java/org/chat/handsondoctor/config/DynamoConfig.java
@@ -1,11 +1,8 @@
 package org.chat.handsondoctor.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,23 +14,18 @@ public class DynamoConfig {
     @Value("${amazon.dynamodb.endpoint}")
     private String amazonDynamoDBEndpoint;
 
-    @Value("${amazon.aws.accessKey}")
-    private String amazonAWSAccessKey;
+    @Value("${amazon.aws.region}")
+    private String region;
 
-    @Value("${amazon.aws.secretKey}")
-    private String amazonAWSSecretKey;
-
-
-    @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(amazonDynamoDB());
-    }
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, "ap-northeast-2"))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey)))
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, region)
+                )
                 .build();
     }
+
+
 }
